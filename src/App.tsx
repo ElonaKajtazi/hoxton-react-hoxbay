63e54856f0bceaa7bb8f2dadd6cc6eb5bfa1df50
Elona Kajtazi <MY_NAME@example.com>
2022-08-03 21:05:15 +0200

    created a product manualy :D

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,30 @@
-import Header from './components/Header'
+import Header from "./components/Header";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    
+  }, []);
   return (
     <>
       <Header />
       <main>
         {/* Create your routes here. Don't forget to install the router package! */}
+        <div className="products-container">
+          <ul className="products-container__list">
+            <li className="product-item">
+              <img
+                src="https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+                alt="bag"
+              />
+              <h3>Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops</h3>
+            </li>
+          </ul>
+        </div>
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
