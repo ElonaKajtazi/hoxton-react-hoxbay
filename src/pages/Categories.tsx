f1bb6c73aec1f974ce5adfbd7b6e109a347fc846
Elona Kajtazi <MY_NAME@example.com>
2022-08-04 10:56:24 +0200

    worked a bit on categories page!!

--- src/pages/Categories.tsx
+++ src/pages/Categories.tsx
@@ -1,5 +1,27 @@
-export function Categories () {
-    return(
-        <h1>Catecories</h1>
-    )
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+export function Categories() {
+  type CategoryType = {
+    id: number;
+    name: string;
+  };
+  const [categories, setcatecories] = useState<CategoryType[]>([]);
+  useEffect(() => {
+    fetch("http://localhost:4000/categories")
+      .then((resp) => resp.json())
+      .then((categoriesFromServer) => setcatecories(categoriesFromServer));
+  }, []);
+  return (
+    <div className="categories-container">
+      {categories.map((category) => (
+        <ul className="categories-container__list">
+            
+          <li>
+            <Link to="/home">{category.name}</Link>
+          </li>
+        </ul>
+      ))}
+    </div>
+  );
 }
